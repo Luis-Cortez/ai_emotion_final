6179b6d72d71e0ceb948ab925400e3acd4922519
Luis-Cortez <messibarbro@gmail.com>
2025-02-23 10:45:23 -0800

    emotion_detector function created and functional

---
+++ emotion_detection.py
@@ -0,0 +1,31 @@
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson.natural_language_understanding_v1 import Features, EmotionOptions
+from dotenv import load_dotenv
+import os
+import json
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+SERVICE_URL = os.getenv("SERVICE_URL")
+
+authenticator = IAMAuthenticator( API_KEY )
+natural_language_understanding = NaturalLanguageUnderstandingV1(
+    version = '2022-04-07',
+    authenticator = authenticator
+)
+
+natural_language_understanding.set_service_url( SERVICE_URL )
+
+def emotion_detector( text_to_analyse ):
+    response = natural_language_understanding.analyze(
+        text = text_to_analyse,
+        features  = Features( emotion = EmotionOptions())
+    ).get_result()
+
+    return json.dumps( response['emotion']['document'], indent = 2)
+   
+text = emotion_detector("I love this new technology")
+
+print( text )
