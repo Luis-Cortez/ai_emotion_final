0012b11887469063c7e9fc2d254f0acdd35087e9
Luis-Cortez <messibarbro@gmail.com>
2025-02-23 20:50:02 -0800

    forgot server.py and test_emotion_detection.py

---
+++ server.py
@@ -0,0 +1,19 @@
+"""importing needed modules"""
+from flask import Flask, render_template, request
+from EmotionDetection import emotion_detector
+
+app = Flask(__name__)
+
+@app.route("/", methods = ['GET'])
+def index() -> str:
+    """rendering index.html template """
+    return render_template("index.html")
+
+@app.route("/emotionDetector", methods = ['GET'])
+def emotion_detect() -> dict:
+    """ Analazing emotions"""
+    text_to_analyze = request.args.get('textToAnalyze')
+    emotions = emotion_detector(text_to_analyze)
+    if emotions["dominant_emotion"] is None:
+        return "Invalid text! Please try again!"
+    return emotions
---
+++ test_emotion_detection.py
@@ -0,0 +1,15 @@
+import unittest
+from EmotionDetection import emotion_detector
+
+class TestEmotionDetection( unittest.TestCase ):
+    def test_1(self):
+        self.assertEqual( emotion_detector("I am glad this happened")["dominant_emotion"], "joy")
+        self.assertEqual( emotion_detector("I am really mad about this")["dominant_emotion"], "anger")
+        self.assertEqual( emotion_detector("I feel disgusted just hearing about this")["dominant_emotion"], "disgust")
+        self.assertEqual( emotion_detector("I am so sad about this")["dominant_emotion"], "sadness")
+        self.assertEqual( emotion_detector("I am really afraid that this will happen")["dominant_emotion"], "fear")
+        self.assertEqual( emotion_detector("I love my life")["dominant_emotion"], "joy")
+
+
+if __name__ == "__main__":
+    unittest.main()
